b972551e7d94aea9ea33f780c289aeb5b82abb5e
Avikumar <aviiiprajapati16@gmail.com>
2025-08-07 15:21:12 +0530

    Maven Wrapper script, NavBar component, and Post entity model

---
+++ src/main/java/com/devconnect/devconnect/model/Post.java
@@ -0,0 +1,45 @@
+package com.devconnect.devconnect.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,columnDefinition  ="TEXT")
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+
+    @ManyToMany(mappedBy = "likedPosts")
+    private Set<User> likedByUsers = new HashSet<>();
+
+   
+
+
+}
